9814c2379d48e54c6d28a460bb2d1e47c6f7c7fa
Liang Zhang <psychelzh@outlook.com>
2020-10-25 20:09:21 +0800

    🐞fix(database): adapt to db upgrade
    The behavior of `UserId` is changed to record `user_organization` `Id`.
    
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- sql/fetch_raw_data.sql
+++ sql/fetch_raw_data.sql
@@ -1,16 +1,16 @@
 SELECT
-  user_profile.Id user_id,
-  user_profile.RealName user_name,
+  `user`.Id user_id,
+  `user`.RealName user_name,
   content.`Name` game_name,
-  content_orginal_data.CreateTime game_time,
+  content_orginal_data_detail.CreateTime game_time,
   content_orginal_data_detail.OrginalData game_data
 FROM
-  content_orginal_data
-  INNER JOIN content_orginal_data_detail
-    ON content_orginal_data_detail.OrginalDataId = content_orginal_data.Id
+  content_orginal_data_detail
   INNER JOIN iquizoo_content_db.content
-    ON content.Id = content_orginal_data.ContentId
-  INNER JOIN iquizoo_user_db.user_profile
-    ON user_profile.Id = content_orginal_data.UserId
+    ON content.Id = content_orginal_data_detail.ContentId
+  INNER JOIN iquizoo_user_db.user_organization
+    ON user_organization.Id = content_orginal_data_detail.UserId
+  INNER JOIN iquizoo_user_db.`user`
+    ON `user`.Id = user_organization.UserId
 WHERE
-  content_orginal_data.OrganizationId = '13bfce3d-4bd0-45ac-ba68-e6021318ecdf';
+  content_orginal_data_detail.OrganizationId = '13bfce3d-4bd0-45ac-ba68-e6021318ecdf';
--- sql/fetch_raw_scores.sql
+++ sql/fetch_raw_scores.sql
@@ -1,6 +1,6 @@
 SELECT
-  user_profile.Id user_id,
-  user_profile.RealName user_name,
+  `user`.Id user_id,
+  `user`.RealName user_name,
   content.`Name` game_name,
   content_score_detail.CreateTime game_time,
   content_score_detail.ApproximateScore raw_score
@@ -8,7 +8,9 @@ FROM
   content_score_detail
   INNER JOIN iquizoo_content_db.content
     ON content.Id = content_score_detail.ContentId
-  INNER JOIN iquizoo_user_db.user_profile
-    ON user_profile.Id = content_score_detail.UserId
+  INNER JOIN iquizoo_user_db.user_organization
+    ON user_organization.Id = content_score_detail.UserId
+  INNER JOIN iquizoo_user_db.`user`
+    ON `user`.Id = user_organization.UserId
 WHERE
   content_score_detail.OrganizationId = '13bfce3d-4bd0-45ac-ba68-e6021318ecdf';
